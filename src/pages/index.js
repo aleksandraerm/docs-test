a892aedad02814c154d431ea9015b64364e9e067
aleksandraerm <aleksandrinaerm@gmail.com>
2023-09-08 18:25:10 +0300

    логотип, главная страница

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Перейти в докуменатцию
+            Перейти в документацию
           </Link>
         </div>
       </div>
